efd4047dee12f79073fcfe4e900499757ab763b8
Alex Lynch <mlynch5187@gmail.com>
2020-02-03 12:58:43 -0700

    Create ship_test

---
+++ lib/ship.rb
@@ -0,0 +1,8 @@
+class Ship
+
+  def initialize(name, health)
+    @name = name
+    @health = health
+  end
+
+end
---
+++ test/ship_test.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < MiniTest::Test
+
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists
+    assert_instance_of Ship, @cruiser
+  end
+
+end
+
+# pry(main)> cruiser = Ship.new("Cruiser", 3)
+# #=> #<Ship:0x00007feb05112d10...>
+#
+# pry(main)> cruiser.name
+# #=> "Cruiser"
+#
+# pry(main)> cruiser.length
+# #=> 3
+#
+# pry(main)> cruiser.health
+# #=> 3
+#
+# pry(main)> cruiser.sunk?
+# #=> false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.health
+# #=> 2
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.health
+# #=> 1
+#
+# pry(main)> cruiser.sunk?
+# #=> false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.sunk?
+# #=> true
