2975f079d748e2aff00313e438312b3838632a37
Alex Lynch <mlynch5187@gmail.com>
2020-02-10 16:09:59 -0700

    Create place_computer_ships method in game class, start place_user_Ships method

--- lib/game.rb
+++ lib/game.rb
@@ -23,10 +23,34 @@ class Game
 
     if user_response == "p"
       play
+    elsif user_response == "q"
+      start
+    else
+      "I didn't recognize that key. Press 'p' to play or 'q' to quit."
   end
 
-  def play
-
-    
+  def place_computer_ships(ship)
+    coordinates = []
+    if @computer_board.valid_placement?(ship, coordinates)
+      @computer_board.place(ship, coordinates)
+    else
+      coordinates = @computer_board.cells.keys.sample(ship.length)
+    until
+      @computer_board.valid_placement?(ship, coordinates)
+      coordinates = @computer_board.cells.keys.sample(ship.length)
+    end
+    @computer_board.place(ship, coordinates)
   end
+
+    if ship == @computer_submarine
+      puts "I have laid out my ships. Now it's your turn."
+      puts "The cruiser is three units long, and the submarine is two units long."
+    end
+  end
+
+  def place_user_ships(ship)
+    puts @user_board.render(true)
+    puts "Enter the coordinates for #{ship.name}. Remember it takes up #{ship.length} units."
+  end
+
 end
